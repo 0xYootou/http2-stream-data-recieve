99f6672a71cb182788b7b874427f054d1a9787a9
undefined <undefined>
2021-12-08 16:47:08 +0800

    hello

--- README.md
+++ README.md
@@ -1,22 +1,11 @@
-# http2-test
+### 安装
 
-
-### 开发环境
-
- - mac: 10.12.6
- - node: v8.9.4
- 
- ### 安装
- 
- ```npm
+```npm
 npm install
 ```
 
 ### 启动
 
-1、输入命令
 ```npm
 npm start
 ```
-
-2、在浏览器打开： https://localhost:8080/
--- src/server.js
+++ src/server.js
@@ -1,45 +1,62 @@
-'use strict'
+"use strict";
 
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 // eslint-disable-next-line
-const http2 = require('http2')
-const helper = require('./helper')
+const http2 = require("http2");
+const helper = require("./helper");
 
-const PORT = process.env.PORT || 8080
-const PUBLIC_PATH = path.join(__dirname, '../public')
+const PORT = process.env.PORT || 8081;
+const PUBLIC_PATH = path.join(__dirname, "../public");
 
-const publicFiles = helper.getFiles(PUBLIC_PATH)
+const publicFiles = helper.getFiles(PUBLIC_PATH);
 
 //创建HTTP2服务器
-const server = http2.createSecureServer({
-  cert: fs.readFileSync(path.join(__dirname, '../ssl/cert.pem')),
-  key: fs.readFileSync(path.join(__dirname, '../ssl/key.pem'))
-}, onRequest)
+const server = http2.createSecureServer(
+  {
+    cert: fs.readFileSync(path.join(__dirname, "../ssl/cert.pem")),
+    key: fs.readFileSync(path.join(__dirname, "../ssl/key.pem")),
+  },
+  onRequest
+);
 
 // Request 事件
-function onRequest (req, res) {
-    // 路径指向 index.html
-  const reqPath = req.url === '/' ? '/index.html' : req.url
-    //获取html资源
-  const file = publicFiles.get(reqPath)
+function onRequest(req, res) {
+  // 路径指向 index.html
+  const reqPath = req.url === "/" ? "/index.html" : req.url;
+  if (reqPath == "/test.json") {
+    let count = 0;
+    const timer = setInterval(() => {
+      count++;
+      if (count > 10) {
+        clearInterval(timer);
+        res.stream.close();
+      } else {
+        res.stream.write('{"count":' + count + "}");
+      }
+    }, 1000);
+    return;
+  }
+  //获取html资源
+  const file = publicFiles.get(reqPath);
 
   // 文件不存在
   if (!file) {
-    res.statusCode = 404
-    res.end()
-    return
+    res.statusCode = 404;
+    res.end();
+    return;
   }
-  
-  res.stream.respondWithFD(file.fileDescriptor, file.headers)
+  res.stream.respondWithFD(file.fileDescriptor, file.headers);
 }
-
+const child_process = require("child_process");
 server.listen(PORT, (err) => {
-  console.log('监听服务器启动=====\n')
+  console.log("监听服务器启动=====\n");
   if (err) {
-    console.error(err)
-    return
+    console.error(err);
+    return;
   }
 
-  console.log(`Server listening on ${PORT}`)
-})
+  console.log(`Server listening on ${PORT}`);
+
+  child_process.exec(`open https://localhost:${PORT}`);
+});
